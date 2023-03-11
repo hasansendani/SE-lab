d98f438595de28225a28889cd73af78f0623451a
hasansendani <h.sendani@digikala.com>
2023-03-11 19:53:26 +0330

    validate user input

--- calculator.py
+++ calculator.py
@@ -1,11 +1,30 @@
+import re
+
 def calculate():
-    get_statement()
+    statement = get_statement()
+    validate_statement(statement)
 
 
 def get_statement():
     print("enter your statement. it should just contain numbers, +, -, *, /")
     statement = input()
     print('your statement is: ' + statement)
+    return statement
+
+def validate_statement(statement):
+    statement = statement.replace('+', ' ')
+    statement = statement.replace('*', ' ')
+    statement = statement.replace('/', ' ')
+    statement = statement.replace('-', ' ')
+    statement = statement.split()
+    for item in statement:
+        if not item.isnumeric():
+            print(statement)
+            print("input is not valid")
+            return False
+    print("input is valid")
+    return True
+
 
 
 
